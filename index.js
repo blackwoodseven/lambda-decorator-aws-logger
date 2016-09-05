58e8458ca8edaff4c6d1da36943e202e2286a53b
Victor Galindo <vg@blackwoodseven.com>
2016-09-05 11:44:10 +0200

    Add initial log

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var awsLogger = require('aws-logger');
 
 module.exports = (handlerFn) => (event, context, callback) => {
-
+  console.log('Starting AWS LOGGER decorator')
   const env = context.environment;
   if (!env)
     throw new Error('No context.environment defined: You can easily fix this by using the set-environment decorator before me!'); // <- Im the most important semi-colon you have ever seen! DO NOT REMOVE ME!
